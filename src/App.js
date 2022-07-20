3c094beab9df3fb36bfe5b90dcf346f7c087f03d
tuannt2000 <tuaniq2000@gmail.com>
2022-07-20 10:25:49 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { DefaultLayout } from "~/layouts";
-import { routes } from "~/routes";
-import DefaultLayoutAdmin from "./layouts/DefaultLayoutAdmin/DefaultLayoutAdmin";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { DefaultLayout } from '~/layouts';
+import { routes } from '~/routes';
+import React, { useState } from "react";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {routes.map((route, index) => {
-            let Layout = DefaultLayout;
-            let LayoutAdmin = DefaultLayoutAdmin;
-            const Page = route.component;
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={
-                  route.role === "admin" ? (
-                    <LayoutAdmin>
-                      <Page />
-                    </LayoutAdmin>
-                  ) : (
-                    <Layout>
-                      <Page />
-                    </Layout>
-                  )
-                }
-              />
-            );
-          })}
-        </Routes>
-      </div>
-    </Router>
-  );
+    const [message, setMessage] = useState(true);
+
+    function saveLogin() {
+        const query = new URLSearchParams(window.location.search);
+        const token = query.get("token");
+        console.log(query, token);
+        if (token && token.length > 0) localStorage.setItem("token", token);
+        setMessage(false);
+        window.location.href = "/";
+    }
+
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {routes.map((route, index) => {
+                        let Layout = DefaultLayout;
+
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={
+                            <Layout>
+                                <Page />
+                            </Layout>}
+                        />
+                    })}
+                    <Route path="/login" element={
+                        <Snackbar open={message} autoHideDuration={1000} onClose={saveLogin}>
+                            <MuiAlert elevation={6} variant="filled" severity="success">SuccessFully Logged In</MuiAlert>
+                        </Snackbar>} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
